6e00e3945e0977d336011801d4a46e0b29d7f1c2
Toni Mertanen <toni.mertanen@k-market.com>
2021-05-17 21:18:26 +0300

    Code base
    Base for starting. Includes examples of code which need to be
    altered for correct output. Read the code and alter.

---
+++ Tilaus.py
@@ -0,0 +1,29 @@
+## Tilaus-luokka
+
+class Tilaus:
+    # Tilauksella on seuraavat tiedot tietokantataulussa nimeltä tilaus:
+    # tilausnro - tilausnumero
+    # pvm       - tilauksen päivämäärä, muodossa ppkkvvvv
+    # asnro     - tilauksen tehneen asiakkaan asiakasnumero
+
+    # Parametrisoitu muodostinfunktio - constructor
+    def __init__(self, connection):
+        self.conn = connection
+        self.cur = self.conn.cursor()
+
+
+    # Haetaan kaikki tilaukset
+    # Tämän sisällä kutsutaan TulostaTilaus()
+    def HaeKaikkiTilaukset(self):
+        try:
+            self.cur.execute("SELECT asiakas.snimi, pvm, tilausnro FROM tilaus, asiakas where asiakas.asnro = tilaus.asnro")
+            rivit = self.cur.fetchall()
+            self.TulostaTilaus(rivit)
+        except Exception as e:
+            print("Rivejä ei pystytty lukemaan tilaus-taulusta: {}.".format(e)) 
+
+    # Tulostetaan 1 asiakas kerrallaan
+    # Tänne parametrina rivit, jotka select on hakenut
+    def TulostaTilaus(self, rivit):
+        for rivi in rivit:
+            print("Asiakas: ", rivi[0], " tilausnumero: ", rivi[2], " tilauspvm:", rivi[1])
--- ratkaisu.py
+++ ratkaisu.py
@@ -1,1 +1,81 @@
-helloWorld
+# Määrittele importit
+import sqlite3
+from sqlite3 import Error
+
+from Asiakas import Asiakas
+from Tilaus import Tilaus
+
+
+# Määrittele luokat
+def create_connection(db_file):
+    ## Esimerkki sivustolta: https://www.sqlitetutorial.net/sqlite-python/ 
+    """ create a database connection to the SQLite database
+        specified by db_file
+    :param db_file: database file
+    :return: Connection object or None
+    """
+    conn = None
+    try:
+        conn = sqlite3.connect(db_file)
+        return conn
+    except Error as e:
+        print(e)
+    return conn
+ 
+
+def valikko():   # Funktio, joka tulostaa valikon, ja palauttaa tehdyn valinnan
+    print("\n1 - tulosta kaikki asiakkaat")
+    print("2 - hae ja tulosta asiakas sukunimen perusteella")
+    print("3 - hae ja tulosta asiakas asiakasnumeron perusteella")
+    print("4 - tulosta kaikki tilaukset")
+    print("5 - tulosta yhden asiakkaan tilaukset")
+    print("0 - lopeta")
+    jatka = input("? ")
+    return jatka
+ 
+
+
+
+### PÄÄOHJELMA ###
+
+## Laita tähän se polku, jossa kauppa.db on omalla koneellasi
+tietokanta = r"kauppa.db"
+
+# luodaan yhteys tietokantaan
+conn = create_connection(tietokanta)
+ 
+if conn is not None:  # Mikäli tietokantayhteys saatiin luotua:
+       
+    jatka = valikko()
+    while jatka != "0":
+        if jatka == "1":
+            asiakas = Asiakas(conn)
+            asiakas.HaeKaikkiAsiakkaat()
+        elif jatka == "2":
+            asiakas = Asiakas(conn)
+            nimi = input("Anna asiakkaan sukunimi: ")
+            asiakas.HaeAsiakasNimella(nimi)
+        elif jatka == "3":
+            asiakas = Asiakas(conn)
+            numero = input("Anna asiakkaan asiakasnumero: ")
+            asiakas.HaeAsiakasNumerolla(numero)
+        elif jatka == "4":
+            tilaus = Tilaus(conn)
+            tilaus.HaeKaikkiTilaukset()
+        elif jatka == "5":
+            asiakas = Asiakas(conn)
+            numero = input("Anna asiakkaan asiakasnumero: ")
+            asiakas.TulostaAsiakkaanTilaukset(numero)
+        else:
+            print("Tarkista valintasi")
+
+        jatka = valikko()
+            
+    conn.close()   # suljetaan yhteys
+
+else:
+    print("Virhe! Yhteyttä tietokantaan ei voida luoda.")
+
+
+
+
